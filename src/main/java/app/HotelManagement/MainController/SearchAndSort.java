a601b67aa74cfe5b48151915ddeffed0110019af
splakash <bilsinak@gmail.com>
2026-01-16 22:07:34 +0530

    Add JWT Authentication filter to Authenticate Apis

--- src/main/java/app/HotelManagement/MainController/SearchAndSort.java
+++ src/main/java/app/HotelManagement/MainController/SearchAndSort.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/admin")
+@RequestMapping("/v1")
 @CrossOrigin(origins = "http://localhost:3000")
 public class SearchAndSort {
 
--- src/main/java/app/HotelManagement/UserManagement/WebConfiguration.java
+++ src/main/java/app/HotelManagement/UserManagement/WebConfiguration.java
@@ -1,5 +1,6 @@
 package app.HotelManagement.UserManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,10 +12,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfiguration {
+
+
+        @Autowired
+        public JwtAuthenticationFilter jwtAuthenticationFilter;
 
         @Bean
         public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
@@ -24,7 +30,9 @@ public class WebConfiguration {
                     .requestMatchers("/v1/admin/**").authenticated()
                     .anyRequest().permitAll()).
                     sessionManagement(session ->
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .addFilterBefore(jwtAuthenticationFilter,
+                    UsernamePasswordAuthenticationFilter.class);
 
             return http.build();
         }
